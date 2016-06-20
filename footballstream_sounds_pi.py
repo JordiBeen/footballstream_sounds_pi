a602efe9bb7bb925c608c7d5545bd124ef83e601
jordibeen <jordibeen1996@gmail.com>
2016-06-20 16:23:10 +0200

    Loop every 5 seconds, works with Android Application

--- footballstream_sounds_pi.py
+++ footballstream_sounds_pi.py
@@ -1,5 +1,5 @@
 #Uncomment if testing on Pi
-#from espeak import espeak
+from espeak import espeak
 
 #Uncomment if testing on MacOS
 #from subprocess import call
@@ -9,10 +9,10 @@ import time
 
 def say(text):
 	#Uncomment if testing on Pi
-	#espeak.set_parameter(espeak.Parameter.Rate, 120)
-	#espeak.synth(text)
-	#while espeak.is_playing():
-	#	time.sleep(0.1)
+	espeak.set_parameter(espeak.Parameter.Rate, 120)
+	espeak.synth(text)
+	while espeak.is_playing():
+		time.sleep(0.1)
 
 	#Uncomment if testing on MacOS
 	#call('say ' + text, shell=True)
@@ -20,23 +20,28 @@ def say(text):
 devthisapi = 'http://devthisapi.jordibeen.nl/api/v1/matches'
 footballstreamapi = 'http://footballstream-api.jordibeen.nl/api/v1/matches'
 
-r = requests.get(devthisapi)
+while True:
+	time.sleep(5)
 
-json_response = r.json()
 
-if 'match' not in json_response:
-	say('Please select matches')
-	exit
+	r = requests.get(devthisapi)
 
-match_ids = json_response['match']['match_ids']
-
-for match_id in match_ids.split(","):
-	say('i am following match with id {}'.format(match_id))
-	r = requests.get('{}/{}'.format(footballstreamapi, match_id))
 	json_response = r.json()
+
+	if 'match' not in json_response:
+		say('Please select matches')
+		continue
+
+	match_ids = json_response['match']['match_ids']
+
+	if not match_ids:
+		say('No matches selected')
+
+	for match_id in match_ids.split(","):
+		say('i am following match with id {}'.format(match_id))
+		r = requests.get('{}/{}'.format(footballstreamapi, match_id))
+		json_response = r.json()
     	
-	if 'match' in json_response:
-        	match = json_response['match']
-		ft_score = match['ft_score'].replace('[', '').replace(']', '').replace('-', ' to ')
-        	say('This game is {}'.format(match['matchup']))
-        	say('This game ended with {}'.format(ft_score))
+		if 'match' in json_response:
+	        	match = json_response['match']
+        		say('This game is {}'.format(match['matchup']))
