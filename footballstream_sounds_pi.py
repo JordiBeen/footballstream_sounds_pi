a9ce14d4918b0d776efae01a243730fbd093cef8
jordibeen <jordibeen1996@gmail.com>
2016-06-23 16:13:03 +0200

    Cleanup and added comments

--- footballstream_sounds_pi.py
+++ footballstream_sounds_pi.py
@@ -1,47 +1,78 @@
-#Uncomment if testing on Pi
-from espeak import espeak
-
-#Uncomment if testing on MacOS
-#from subprocess import call
-
 import requests
 import time
 
-def say(text):
-	#Uncomment if testing on Pi
-	espeak.set_parameter(espeak.Parameter.Rate, 120)
-	espeak.synth(text)
-	while espeak.is_playing():
-		time.sleep(0.1)
+from espeak import espeak
+from subprocess import call
 
-	#Uncomment if testing on MacOS
-	#call('say ' + text, shell=True)
 
+# DECLARING GLOBAL VARIABLES #
+
+# Set the following
+# 	True when running on a Raspberry Pi
+# 	False when running on Mac OSX
+runningOnPi = False
+
+# API URl's
 devthisapi = 'http://devthisapi.jordibeen.nl/api/v1/matches'
 footballstreamapi = 'http://footballstream-api.jordibeen.nl/api/v1/matches'
 
+
+# This will cause the application to convert text to speech
+# It checks whether the application is ran on Pi or MacOS, and
+# will speak according to it.
+def say(text):
+	if runningOnPi:
+		espeak.set_parameter(espeak.Parameter.Rate, 120)
+		espeak.synth(text)
+		while espeak.is_playing():
+			time.sleep(0.1)
+	else:
+		call('say ' + text, shell=True)
+
+
+# Function to perform a request to api given
+# as param, and return the json response
+def json_request(api):
+	r = requests.get(api)
+	json_response = r.json()
+	return json_response
+
+
+# Continuous loop starts here
 while True:
+	# The system will sleep for a random between 0-9 seconds,
+	# causing the application to appear more natural
 	time.sleep(5)
 
+	# Perform a request to the D3VTH1S API
+	devthisapi_json_response = json_request(devthisapi)
 
-	r = requests.get(devthisapi)
-
-	json_response = r.json()
-
-	if 'match' not in json_response:
+	# If there are no matches
+	if 'match' not in devthisapi_json_response:
+		# Tell the user to select matches
 		say('Please select matches')
 		continue
 
-	match_ids = json_response['match']['match_ids']
+	# Get the matchid's that the user has selected
+	match_ids = devthisapi_json_response['match']['match_ids']
 
+	# If there are no match_ids given
 	if not match_ids:
+		# Tell the user no matches are selected
 		say('No matches selected')
 
+	# For every match_id that the user has selected
 	for match_id in match_ids.split(","):
-		say('i am following match with id {}'.format(match_id))
-		r = requests.get('{}/{}'.format(footballstreamapi, match_id))
-		json_response = r.json()
-    	
-		if 'match' in json_response:
-	        	match = json_response['match']
-        		say('This game is {}'.format(match['matchup']))
+		# Tell the user I am following the match
+		say('i am following match with ID {}'.format(match_id))
+
+		# Perform a request to the FootballStream API
+		# with the match_id as parameter
+		footballstreamapi_json_response = json_request('{}/{}'.format(
+			footballstreamapi, match_id))
+
+		# If there is a match found for the ID
+		if 'match' in footballstreamapi_json_response:
+			match = footballstreamapi_json_response['match']
+			# Tell the use the matchup he has selected
+			say('This game is {}'.format(match['matchup']))
